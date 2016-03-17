73d77e4b9361155d92b776a2fce43523931bd779
Lionel Benedetti <lionel@cblue.be>
2016-03-17 07:30:32 +0100

    better free memeory display

--- freem.sh
+++ freem.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-free | grep Mem | awk '{print $4/$2 * 100.0}' |cut -c 1-4
+f=$(free | grep + | awk '{print $4}')
+u=$(free | grep Mem | awk '{print $2}')
+echo "scale=5; $f/$u * 100" | bc | cut -c 1-4
 #echo "$(free -m | grep Mem | awk '{print $4}')/$(free -m | grep buffers/ | awk '{print $4}')/$(free -m | grep Mem | awk '{print $2}'"
