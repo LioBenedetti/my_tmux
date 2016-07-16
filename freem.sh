646ea37aa29db0ef3a497db610afed7d963ac473
Lionel Benedetti <lionel@cblue.be>
2016-07-16 22:45:38 +0200

    ~ Better mem display

--- freem.sh
+++ freem.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-f=$(free | grep + | awk '{print $4}')
+f=$(free | grep Mem | awk '{print $4}')
 u=$(free | grep Mem | awk '{print $2}')
 echo "scale=5; $f/$u * 100" | bc | cut -c 1-4
 #echo "$(free -m | grep Mem | awk '{print $4}')/$(free -m | grep buffers/ | awk '{print $4}')/$(free -m | grep Mem | awk '{print $2}'"
